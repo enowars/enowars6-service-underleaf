c2d1245cbac05f0749cdd0934bcb2a0ea03dd291
TheHllm <hllmollenkopf@gmail.com>
2022-05-05 21:38:43 +0200

    Added commit

---
+++ backend/src/git/commit.ts
@@ -0,0 +1,14 @@
+import shellescape from 'shell-escape';
+
+import { RequestHandler } from "express";
+import { asyncExec } from "../helpers/git";
+import { getProjectPath } from "../helpers/project";
+import { status_ok } from "../helpers/status";
+
+export const commitProject:RequestHandler = async (req, res) => {
+    
+    const path = getProjectPath(req.params.id);
+    await asyncExec(`cd ${path}; git add . && git commit -m ${shellescape([req.body.message])} || true`);
+
+    res.send(status_ok);
+}
--- backend/src/git/router.ts
+++ backend/src/git/router.ts
@@ -2,13 +2,16 @@ import express from "express";
 import { loggedIn } from "../auth/loggedIn";
 import { reqProjectIdIsSafe } from "../helpers/project";
 import { modifyNeeded } from "../project/modifyNeeded";
+import { commitProject } from "./commit";
+import { pullProject } from "./pull";
 import { pushProject } from "./push";
 
 export const router = express.Router();
 
-router.use(loggedIn, reqProjectIdIsSafe, modifyNeeded);
+const checks = [loggedIn, reqProjectIdIsSafe, modifyNeeded];
 
-router.get('/push/:id/', pushProject);
-router.get('/pull/:id/', pushProject);
+router.get('/push/:id/', checks, pushProject);
+router.get('/pull/:id/', checks, pullProject);
+router.post('/commit/:id/', checks, commitProject);
 
 export default router;

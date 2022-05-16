0fd24f8c7d127643a91cf48f73afad967c403b00
TheHllm <hllmollenkopf@gmail.com>
2022-05-17 01:52:22 +0200

    More secure docker container

--- service/backend/src/latex/compile.ts
+++ service/backend/src/latex/compile.ts
@@ -63,6 +63,8 @@ export const compileProject: RequestHandler = async (req, res) => {
     Image: latexDockerImage,
     WorkingDir: "/data",
     Cmd: ["pdflatex", "-shell-escape", "/data/" + req.body.file],
+    User: '1000:1000',
+    NetworkDisabled: true,
   });
 
   const tarPath = "/tmp/" + req.params.id + ".tar";

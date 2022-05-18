450e1ebad28dc870240fe1c0fa6c998ca4908998
TheHllm <hllmollenkopf@gmail.com>
2022-05-18 13:26:34 +0200

    Removed debug logging

--- service/backend/src/git/commit.ts
+++ service/backend/src/git/commit.ts
@@ -6,7 +6,6 @@ import { status_ok } from "../helpers/status";
 export const commitProject: RequestHandler = async (req, res) => {
   const path = getProjectPath(req.params.id);
 
-  console.log(req.body.message);
   await gitCommit(path, req.body.message);
 
   res.send(status_ok);

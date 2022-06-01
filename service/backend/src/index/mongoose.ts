a0a095695328ed55c4c2597610d64f937eb3b641
TheHllm <hllmollenkopf@gmail.com>
2022-06-01 20:53:00 +0200

    Fixed Formatting...

--- service/backend/src/index/mongoose.ts
+++ service/backend/src/index/mongoose.ts
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const username = encodeURIComponent(process.env["MONGO_INITDB_ROOT_USERNAME"] || "");
-const passowrd = encodeURIComponent(process.env["MONGO_INITDB_ROOT_PASSWORD"] || "");
+const username = encodeURIComponent(
+  process.env["MONGO_INITDB_ROOT_USERNAME"] || ""
+);
+const passowrd = encodeURIComponent(
+  process.env["MONGO_INITDB_ROOT_PASSWORD"] || ""
+);
 
-export const connection = mongoose.connect(`mongodb://${username}:${passowrd}@db`);
+export const connection = mongoose.connect(
+  `mongodb://${username}:${passowrd}@db`
+);

3d899dc8f556bfd8340cad22658c6feb658f0da9
TheHllm <hllmollenkopf@gmail.com>
2022-06-22 21:53:34 +0200

    Fixed check may run forever

--- service/backend/src/helpers/checkSymlinkedPath.ts
+++ service/backend/src/helpers/checkSymlinkedPath.ts
@@ -5,13 +5,24 @@ import { exists, lexists } from "./existsAsync";
 // checks if a path (containing symlinks) resolves to a path containing a specific prefix (uses realpath, but dose not error if a suffix of the path dose not exist).
 export async function symlinkPathResolvesTo(
   path: string,
-  prefix: string
+  prefix: string,
+  seenPaths: Set<string> = new Set()
 ): Promise<boolean> {
   let rpath = resolve(path);
+  if(seenPaths.has(rpath)){
+    console.log("seen", rpath, seenPaths);
+    return false;
+  }
+  seenPaths.add(rpath);
 
   // realpath needs the path to exist -> chop off stuff from the end, until it does or a dangeling symlink is found
   while (!(await lexists(rpath))) {
     rpath = resolve(rpath, "..");
+    if(seenPaths.has(rpath)){
+      console.log("seen", rpath, seenPaths);
+      return false;
+    }
+    seenPaths.add(rpath);
   }
 
   if (!(await exists(rpath))) {
@@ -19,14 +30,15 @@ export async function symlinkPathResolvesTo(
     const target = await fs.readlink(rpath);
 
     if (target.startsWith("/")) {
-      return await symlinkPathResolvesTo(target, prefix);
+      return await symlinkPathResolvesTo(target, prefix, seenPaths);
     } else {
       rpath = resolve(rpath, "..", target); // resolve a relative link, '..' consumes the links name from rpath
-      return await symlinkPathResolvesTo(rpath, prefix); // this path may now contain symlinks again...
+      return await symlinkPathResolvesTo(rpath, prefix, seenPaths); // this path may now contain symlinks again...
     }
   } else {
     // resolve our path that now actually exists
     rpath = await fs.realpath(rpath);
+    seenPaths.add(rpath);
   }
 
   return rpath.startsWith(await fs.realpath(prefix)); // finally check if our prefix is present!
